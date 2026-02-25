b293586e87cd95f4d42e9e2ac00e018a3d311539
barrenoaths <barrenoaths@proton.me>
2026-02-25 20:01:30 +0100

    adding some stuff

---
+++ SumFourNums/SumFourNums.go
@@ -0,0 +1,5 @@
+package SumFourNums
+
+func SumFour(q, w, e, r int) int {
+	return q + w + e + r
+}
--- learnPackaging.go
+++ learnPackaging.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"learnPackaging/SumFourNums"
 	"learnPackaging/SumThreeNums"
 )
 
 func main() {
 	fmt.Println("test")
 	fmt.Println(SumThreeNums.SumThree(1, 2, 3))
+	fmt.Println(SumFourNums.SumFour(1, 2, 3, 4))
 }
